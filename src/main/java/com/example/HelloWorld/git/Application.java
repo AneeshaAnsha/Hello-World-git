29295799026a87165dc2b7ab84876657c930a680
14759 <aneeshaansha01@gmail.com>
2023-03-09 16:10:29 -0500

    Added extra log information

--- src/main/java/com/example/HelloWorld/git/Application.java
+++ src/main/java/com/example/HelloWorld/git/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("HelloWorld Application is started...")
 	}
 
 }
